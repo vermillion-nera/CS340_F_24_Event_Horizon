a2e7ed51d7cfc31a425cbb0603b0ba9f0343b8e8
Sharpy796 <christian.bankovic@gmail.com>
2024-11-13 20:46:51 -0600

    main.py light pseudocode

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("hello world")
 print("sharpy wuz here >:D")
+
+# Ask the user for a command
+# Implement that command, whether it's printing something to the console or exporting a file
+# Repeat until the user exits the program

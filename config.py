7f78d5d67f38db0f1e541d53e23e0884fa7f1612
Sharpy796 <christian.bankovic@gmail.com>
2024-11-19 18:51:00 -0600

    Added templates to main and config

--- config.py
+++ config.py
@@ -1,2 +1,83 @@
-# config constants
-TESTING_CONST = True
+#%% MODULE BEGINS
+module_name = '<***>'
+
+'''
+Version: <***>
+
+Description:
+    <***>
+
+Authors:
+    <***>
+
+Date Created     :  <***>
+Date Last Updated:  <***>
+
+Doc:
+    <***>
+
+Notes:
+    <***>
+'''
+
+#%% IMPORTS                    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if __name__ == "__main__":
+   import os
+   #os.chdir("./../..")
+#
+
+#custom imports
+
+
+#other imports
+from   copy       import deepcopy as dpcpy
+
+'''
+from   matplotlib import pyplot as plt
+import mne
+import numpy  as np 
+import os
+import pandas as pd
+import seaborn as sns
+'''
+#%% USER INTERFACE              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONSTANTS                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% CONFIGURATION               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#%% INITIALIZATIONS             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#%% DECLARATIONS                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Global declarations Start Here
+
+
+
+#Class definitions Start Here
+
+
+
+#Function definitions Start Here
+def main():
+    pass
+#
+
+#%% MAIN CODE                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main code start here
+
+
+
+#%% SELF-RUN                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Main Self-run block
+if __name__ == "__main__":
+    
+    print(f"\"{module_name}\" module begins.")
+    
+    #TEST Code
+    main()

5b8355905d9571dff6a6d453d911def57dca39b9
Nera <57953845+vermillion-nera@users.noreply.github.com>
2024-11-30 15:02:03 -0600

    Revert "RENAMED MODULES AND CLASSES AND MOVED EVERYTHING INTO APPROPIATE FOLDER STRUCTURE"
    This reverts commit 4688bd5694b977080a4d253856b6b9c66d7768ba.

--- class_1.py
+++ class_1.py
@@ -1,5 +1,5 @@
 #%% MODULE BEGINS
-module_name = "dataFrameModule"
+module_name = "class_1"
 
 '''
 Version: 1.0
@@ -15,7 +15,7 @@ Authors:
     Brian Britton
 
 Date Created     :  11/20/2024
-Date Last Updated:  11/26/2024
+Date Last Updated:  11/25/2024
 
 Doc:
     <***>
@@ -31,10 +31,11 @@ if __name__ == "__main__":
 #
 
 #custom imports
-import config
+
 
 #other imports
 from   copy       import deepcopy as dpcpy
+import config
 import numpy  as np
 import pandas as pd
 import logging
@@ -72,11 +73,11 @@ logger = logging.getLogger(__name__)
 
 
 #Class definitions Start Here
-class dataFrameHandler:
+class parentCSV:
     def __init__(self, dataframe):
         self.df = dataframe
         self.shaped = dataframe
-        print("dataFrameHandler initialized.")
+        print("CSV parent initialized.")
     #end
 
     def getDataFrame(self):
@@ -182,7 +183,7 @@ class dataFrameHandler:
             plt.xlabel(column_name)
             plt.ylabel("Frequency")
             if save:
-                plt.savefig(f"OUTPUT/{column_name}_histogram.png")
+                plt.savefig(f"Plots/{column_name}_histogram.png")
             else:
                 plt.show()
         elif self.df[column_name].dtype in ['object', 'category', 'bool']:
@@ -192,7 +193,7 @@ class dataFrameHandler:
             plt.xlabel(column_name)
             plt.ylabel("Frequency")
             if save:
-                plt.savefig(f"OUTPUT/{column_name}_bar_chart.png")
+                plt.savefig(f"Plots/{column_name}_bar_chart.png")
             else:
                 plt.show()
     #end
@@ -200,10 +201,10 @@ class dataFrameHandler:
     # TODO: Add a line plot function
 #end
 
-class csvHandler(dataFrameHandler):
-    def __init__(self, csv_path):
-        self.filepath = csv_path
-        super().__init__(pd.read_csv(csv_path))
+class childCSV(parentCSV):
+    def __init__(self, csv): # TODO: Rename this to csv_path
+        self.filepath = csv
+        super().__init__(pd.read_csv(csv))
         print("CSV child initialized.")
     #end
 
